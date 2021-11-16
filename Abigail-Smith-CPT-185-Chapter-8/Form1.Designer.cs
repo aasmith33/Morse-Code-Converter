cbd5583244e9ef9fcb0e6af38aefc48d654e0a6f
aasmith33 <76235968+aasmith33@users.noreply.github.com>
2021-11-15 21:37:06 -0500

    Add project files.

---
+++ Abigail-Smith-CPT-185-Chapter-8/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Abigail_Smith_CPT_185_Chapter_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the text that you want to be converted to Morse Code:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(45, 59);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(445, 22);
+            this.txtInput.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtInput, "Input text that you want converted to Morse Code");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Location = new System.Drawing.Point(124, 381);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 163);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(155, 98);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(102, 35);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Ex&it";
+            this.toolTip1.SetToolTip(this.btnExit, "Click to exit");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(15, 98);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 35);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "C&lear";
+            this.toolTip1.SetToolTip(this.btnClear, "Click to clear ");
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(87, 35);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(102, 35);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "&Convert";
+            this.toolTip1.SetToolTip(this.btnConvert, "Click to convert");
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(124, 97);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(287, 269);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnConvert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(552, 556);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Morse Code Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Abigail-Smith-CPT-185-Chapter-8/Form1.cs
@@ -0,0 +1,115 @@
+﻿// This program will translate your input into morse code
+// Abigail Smith 2021
+using System;
+using System.Windows.Forms;
+
+namespace Abigail_Smith_CPT_185_Chapter_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtInput.Focus();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string userInput = txtInput.Text;
+            Boolean value = true;
+            string codeOutput = " ";
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput.Contains("0"))
+                {
+                    txtOutput.Text = "-----";
+                }
+                if (userInput.Contains("1"))
+                {
+                    txtOutput.Text = ".----";
+                }
+                if (userInput.Contains("2"))
+                {
+                    txtOutput.Text = "..---";
+                }
+                if (userInput.Contains("3"))
+                {
+                    txtOutput.Text = "...--";
+                }
+                if (userInput.Contains("4"))
+                {
+                    txtOutput.Text = "....-";
+                }
+                if (userInput.Contains("5"))
+                {
+                    txtOutput.Text = ".....";
+                }
+                if (userInput.Contains("6"))
+                {
+                    txtOutput.Text = "-....";
+                }
+                if (userInput.Contains("7"))
+                {
+                    txtOutput.Text = "--...";
+                }
+                if (userInput.Contains("8"))
+                {
+                    txtOutput.Text = "---..";
+                }
+                if (userInput.Contains("9"))
+                {
+                    txtOutput.Text = "----.";
+                }
+
+                switch (char.ToUpper(userInput[i]))
+                {
+                    case ' ': codeOutput += " " + "\r\n"; break;
+                    case ',': codeOutput += "--..--" + "\r\n"; break;
+                    case '.': codeOutput += ".-.-.-" + "\r\n"; break;
+                    case '?': codeOutput += "..--.." + "\r\n"; break;
+                    case 'A': codeOutput += ".-" + "\r\n"; break;
+                    case 'B': codeOutput += "-..." + "\r\n"; break;
+                    case 'C': codeOutput += "-.-." + "\r\n"; break;
+                    case 'D': codeOutput += "-.." + "\r\n"; break;
+                    case 'E': codeOutput += "." + "\r\n"; break;
+                    case 'F': codeOutput += "..-." + "\r\n"; break;
+                    case 'G': codeOutput += "--." + "\r\n"; break;
+                    case 'H': codeOutput += "...." + "\r\n"; break;
+                    case 'I': codeOutput += ".." + "\r\n"; break;
+                    case 'J': codeOutput += ".---" + "\r\n"; break;
+                    case 'K': codeOutput += "-.-" + "\r\n"; break;
+                    case 'L': codeOutput += ".-.." + "\r\n"; break;
+                    case 'M': codeOutput += "---" + "\r\n"; break;
+                    case 'N': codeOutput += "-." + "\r\n"; break;
+                    case 'O': codeOutput += "---" + "\r\n"; break;
+                    case 'P': codeOutput += ".--." + "\r\n"; break;
+                    case 'Q': codeOutput += "--.-" + "\r\n"; break;
+                    case 'R': codeOutput += ".-." + "\r\n"; break;
+                    case 'S': codeOutput += "..." + "\r\n"; break;
+                    case 'T': codeOutput += "-" + "\r\n"; break;
+                    case 'U': codeOutput += "..-" + "\r\n"; break;
+                    case 'V': codeOutput += "...-" + "\r\n"; break;
+                    case 'W': codeOutput += ".--" + "\r\n"; break;
+                    case 'X': codeOutput += "-..-" + "\r\n"; break;
+                    case 'Y': codeOutput += "-.--" + "\r\n"; break;
+                    case 'Z': codeOutput += "--.." + "\r\n"; break;
+                    default: codeOutput += char.ToUpper(userInput[i]); break;
+                }
+                txtOutput.Text = codeOutput;
+            }
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            txtOutput.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
